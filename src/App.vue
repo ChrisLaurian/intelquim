7020b8f7ae0d33cd6bc69e8029927d4db5518dc7
Claurian <134958457+Claurian@users.noreply.github.com>
2023-08-28 13:09:25 -0600

    Modulo lector codigos de barra completado

--- src/App.vue
+++ src/App.vue
@@ -28,4 +28,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.drawingBuffer{
+    display: none;
+  }
 </style>
--- src/JS/BarcodeScannerLogic.js
+++ src/JS/BarcodeScannerLogic.js
@@ -4,50 +4,43 @@ export default {
   data() {
     return {
       scannedBarcode: '',
-      scanning: false,
-      scannerInitialized: false,
     };
   },
   methods: {
-    initScanner() {
-      if (!this.scannerInitialized) {
-        Quagga.init(
-          {
-            inputStream: {
-              name: 'Live',
-              type: 'LiveStream',
-              target: '#scanner-container',
-            },
-            decoder: {
-              readers: ['ean_reader'],
-            },
-          },
-          (err) => {
-            if (err) {
-              console.error('Error al inicializar Quagga:', err);
-              return;
-            }
-            this.scannerInitialized = true;
-          }
-        );
-      }
-    },
     startScan() {
-      if (!this.scanning) {
-        this.scanning = true;
-        this.initScanner();
-        Quagga.start();
-        Quagga.onDetected(this.onBarcodeDetected);
-      }
+      const scannerContainer = document.getElementById('scanner-container');
+      Quagga.init(
+        {
+          inputStream: {
+            name: 'Live',
+            type: 'LiveStream',
+            target: scannerContainer,
+          },
+          decoder: {
+            readers: [
+              'ean_reader',
+              'code_128_reader',
+              'code_39_reader'
+              // Agrega aquí otros lectores que desees
+            ],
+          },
+        },
+        (err) => {
+          if (err) {
+            console.error('Error al inicializar Quagga:', err);
+            return;
+          }
+          Quagga.start();
+          Quagga.onDetected(this.onBarcodeDetected);
+        }
+      );
     },
     onBarcodeDetected(result) {
-        console.log('Resultado de Quagga:', result); // Muestra el objeto de resultado en la consola
-        if (result && result.codeResult && result.codeResult.attachData) {
-          this.scannedBarcode = result.codeResult.attachData;
-          this.stopScan();
-        } else {
-          console.error('Resultado de Quagga no válido');
-        }
-      },
+      this.scannedBarcode = result.codeResult.code;
+      Quagga.stop();
+    },
+  },
+  beforeUnmount() {
+    Quagga.stop();
   },
 };
